c21a379ef6583fc8b7dbff32aac74cffe7f59205
marcu <marcu@LAPTOP-0NMUG03A.local>
2020-12-01 12:03:54 +0100

    Update dijkstras.py

--- code/dijkstras.py
+++ code/dijkstras.py
@@ -21,7 +21,7 @@ q_min           = 0
 u_max           = 4
 i_max           = 4
 T               = 12
-t               = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+t1               = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 p_t             = [20, 22, 25, 18, 15, 15, 20, 19, 21, 12, 22, 25]
 r               = 0.04
 
@@ -34,7 +34,7 @@ til at beregne de reelle værdier for forward priserne'''
 
 def interest():                         #Danner liste med diskonteringsværdier
     interest_rate = []
-    for i in t:
+    for i in range(T):
         interest_i = np.e**(-r * i /T) 
         interest_rate.append(interest_i)
     return interest_rate
@@ -42,10 +42,11 @@ def interest():                         #Danner liste med diskonteringsværdier
 
 def get_interest_price():               #Danner liste med reelle værdier
     interest_price = []
-    for i in range (len(t)):
+    for i in range (T):
         a = interest()[i]*p_t[i]
         interest_price.append(a)
     return interest_price
+
 
 
 
@@ -53,8 +54,33 @@ def get_interest_price():               #Danner liste med reelle værdier
 af knuder. Da vi ved at der er 12 måneder om vores beholding kan være fra 0 til 11
 ved vi at der i grafen må være 12*11 knuder plus startknuden og slutknuden (når 
 vi sælger til lejer'''
-amount_vertices = 12 * 11 + 2
-matrix = np.zeros((amount_vertices, amount_vertices))    #Laver en tom matrix 
-print(matrix)
 
+def make_matrix(q_t):
+    interest_price = get_interest_price()
+    amount_vertices = 12 * 11 + 2
+    matrix = np.zeros((amount_vertices, amount_vertices))
+    max_border = q_t + i_max
+    if max_border > q_max:
+        max_border = q_max
+    min_border = q_t - u_max
+    if min_border < q_min:
+        min_border = q_min
+    for t in range (T):
+        for i in range (amount_vertices-10):
+            if matrix[t][i] == 0:
+                for j in range(min_border, max_border):
+                    matrix[t][i+j] = interest_price[t]
+    return matrix
+
+print(make_matrix(0))
+        
+            
+    
+    
+    
+    
+    
+    
+    
+    
 
