293430298e1f9f103636d85adc7f93afa0b7c9b7
marcu <marcu@LAPTOP-0NMUG03A.local>
2020-12-03 09:51:57 +0100

    Update den_gode_dijkstras_som_vi_skal_bruge.py, dijkstras.py, and 4 more files...

--- code/dijkstras.py
+++ code/dijkstras.py
@@ -1,86 +1,98 @@
 # -*- coding: utf-8 -*-
 """
-Created on Thu Nov 26 11:30:15 2020
+Created on Wed Dec  2 12:03:31 2020
 
 @author: marcu
 """
 
+from collections import defaultdict
 import numpy as np
-import networkx as nx
-import matplotlib.pyplot as plt
-
-'''Vi vil nu implementere Dijkstras algoritme i python, således vi kan bruge 
-den til at løse vores problem i dets rigtige størrelse.
-Vi vil først indsætte dataen i python og finde en måde hvorpå python kan generere
-vores data uden at vi manuelt skal skrive alle kantvægte ind.'''
-
-'''Indsætning af data'''
-q_0             = 5
-q_max           = 10
-q_min           = 0
-u_max           = 4
-i_max           = 4
-T               = 12
-t1               = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-p_t             = [20, 22, 25, 18, 15, 15, 20, 19, 21, 12, 22, 25]
-r               = 0.04
-
-
-'''Vi vil nu finde de reelle værdier for forwardpriserne i forhold til nutidsværdien.
-Vi laver først en liste med interest som udregner hvor meget mere værd penge er
-til tiden t med vores diskonteringsfaktor r. Derefter kan vi bruge denne liste 
-til at beregne de reelle værdier for forward priserne'''
+ 
+q_max       = 10                #Maks beholdning i lager
+q_min       = 0                 #Minimum beholdning i lager
+i_max       = 4                 #Maks koeb pr. tid
+u_max       = 4                 #Maks salg pr. tid
+T           = 12                #Antal tidsperioder
+q_0         = 5                 #Start beholdning
+q_T         = 0                 #Slut beholdning
+p_0         = 0                 #Start kapital
+r           = 0.04              #Diskonteringsfaktor
+p           = [20, 22, 25, 18, 15, 15, 20, 19, 21, 12, 22 ,25]      
+                                #Forward priser
+disc        = [np.e**(-r*t/T) for t in range(1, T+1)]               
+                                #Diskonteringsvaerdi
+p_disc      = [p[i]*disc[i] for i in range(T)]                      
+                                #Værdi efter diskonteringsfaktor
+infinity    = 9999              #Bruges som substitut for reelt uendelig
 
 
-def interest():                         #Danner liste med diskonteringsværdier
-    interest_rate = []
+
+def addEdge(graph,u,v,e):
+    if u in graph:
+        graph[u].update( {v : e} )
+    else:
+        graph[u] = {v : e}
+
+
+
+def graph_dict():
+    graph = defaultdict(list)
+    vertices = ['q00']
     for i in range(1,T+1):
-        interest_i = np.e**(-r * i /T) 
-        interest_rate.append(interest_i)
-    return interest_rate
+        vertices.append(['q' + str(i) +'.'+ str(j) for j in range (q_max+1)])
+    vertices.append('q_slut')
+    for i in range(T+1):
+        if i == 0:
+            for j in range(-u_max, i_max+1):
+                addEdge(graph, vertices[i][0], vertices[i+1][q_0 + j], round(j*p_disc[i],2))   
+        elif i == 12:
+            for k in range(q_min, q_max+1):
+                addEdge(graph, vertices[i][k], vertices[i+1][0],round(k*p_disc[i-1],2))
+        else:
+            for k in range(q_min, q_max+1):
+                current_units = k
+                min_units = current_units - u_max
+                if min_units < q_min:
+                    min_units = q_min
+                max_units = current_units + i_max
+                if max_units > q_max:
+                    max_units = q_max
+                for j in range(min_units, max_units+1):
+                    addEdge(graph, vertices[i][k], vertices[i+1][j],round((j-k)*p_disc[i],2))
+    return graph
+
+print(graph_dict())
 
 
-def get_interest_price():               #Danner liste med reelle værdier
-    interest_price = []
-    for i in range (T):
-        a = interest()[i]*p_t[i]
-        interest_price.append(a)
-    return interest_price
-
-print(get_interest_price())
 
 
-'''Vi vil nu generere en nabomatrix til grafen. Vi starter med at finde mængden
-af knuder. Da vi ved at der er 12 måneder om vores beholding kan være fra 0 til 11
-ved vi at der i grafen må være 12*11 knuder plus startknuden og slutknuden (når 
-vi sælger til lejer'''
 
-def make_matrix(q_t):
-    interest_price = get_interest_price()
-    amount_vertices = 12 * 11 + 2
-    matrix = np.zeros((amount_vertices, amount_vertices))
-    max_border = q_t + i_max
-    if max_border > q_max:
-        max_border = q_max
-    min_border = q_t - u_max
-    if min_border < q_min:
-        min_border = q_min
-    for t in range (T):
-        for i in range (amount_vertices-10):
-            if matrix[t][i] == 0:
-                for j in range(min_border, max_border):
-                    matrix[t][i+j] = interest_price[t]
-    return matrix
 
-# print(make_matrix(0))
-        
-            
-    
-    
-    
-    
-    
-    
-    
-    
 
+
+
+
+
+
+
+
+
+
+
+
+
+# print(graph_dict()['q'])
+
+
+# def generate_edges(graph):
+#     edges = []
+ 
+#     # for each node in graph
+#     for node in graph:
+         
+#         # for each neighbour node of a single node
+#         for neighbour in graph[node]:
+             
+#             # if edge exists then append
+#             edges.append((node, neighbour))
+#     return edges
