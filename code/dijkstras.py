29f068406ff0fb3157df28aee242cc0b060b866c
marcu <marcu@LAPTOP-0NMUG03A.local>
2020-12-03 12:30:15 +0100

    Update dijkstras.py

--- code/dijkstras.py
+++ code/dijkstras.py
@@ -7,11 +7,29 @@ Created on Wed Dec  2 12:03:31 2020
 
 from collections import defaultdict
 import numpy as np
- 
-q_max       = 10                #Maks beholdning i lager
-q_min       = 0                 #Minimum beholdning i lager
-i_max       = 4                 #Maks koeb pr. tid
-u_max       = 4                 #Maks salg pr. tid
+
+# #Basis problem
+# q_max       = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10]                
+#                                 #Maks beholdning i lager
+# q_min       = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]                 
+#                                 #Minimum beholdning i lager
+# i_max       = [4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4]                 
+#                                 #Maks koeb pr. tid
+# u_max       = [4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4]                    
+#                                 #Maks salg pr. tid
+
+#Udvidet problem
+q_max       = [10, 10, 10, 10, 8, 8, 8, 8, 10, 10, 10, 10]                
+                                #Maks beholdning i lager
+q_min       = [0, 0, 0, 4, 4, 6, 6, 4, 4, 0, 0, 0]                 
+                                #Minimum beholdning i lager
+i_max       = [4, 4, 2, 2, 1, 1, 1, 1, 2, 2, 4, 4]                 
+                                #Maks koeb pr. tid
+u_max       = [4, 4, 2, 2, 1, 1, 1, 1, 2, 2, 4, 4]                    
+                                #Maks salg pr. tid
+p_goal      = 5                 #Lagerbeholdnings aftale
+alpha       = 0.07              #Straffaktor
+                                
 T           = 12                #Antal tidsperioder
 q_0         = 5                 #Start beholdning
 q_T         = 0                 #Slut beholdning
@@ -26,7 +44,6 @@ p_disc      = [p[i]*disc[i] for i in range(T)]
 infinity    = 9999              #Bruges som substitut for reelt uendelig
 
 
-
 def addEdge(graph,u,v,e):
     if u in graph:
         graph[u].update( {v : e} )
@@ -34,35 +51,78 @@ def addEdge(graph,u,v,e):
         graph[u] = {v : e}
 
 
-
 def graph_dict():
     graph = defaultdict(list)
-    vertices = ['q0.0']
+    vertices = ['q0.5']
     for i in range(1,T+1):
-        vertices.append(['q' + str(i) +'.'+ str(j) for j in range (q_max+1)])
+        vertices.append(['q' + str(i) +'.'+ str(j) for j in range (q_max[i-1]+1)])
     vertices.append('q_slut')
-    print(vertices)
     for i in range(T+1):
         if i == 0:
-            for j in range(-u_max, i_max+1):
-                addEdge(graph, vertices[i], vertices[i+1][q_0 + j], round(j*p_disc[i],2))   
+            for j in range(-u_max[i], i_max[i]+1):
+                addEdge(graph, vertices[i], vertices[i+1][q_0 + j], round(j*p_disc[i]+250,2))   
         elif i == 12:
-            for k in range(q_min, q_max+1):
-                addEdge(graph, vertices[i][k], vertices[i+1],round(k*p_disc[i-1],2))
+            for k in range(q_min[i-1], q_max[i-1]+1):
+                if k == p_goal:
+                    addEdge(graph, vertices[i][k], vertices[i+1],round(-k*p_disc[i-1]+250,2))
+                else:
+                    addEdge(graph, vertices[i][k], vertices[i+1],round(-k*p_disc[i-1]+250,2)/alpha)
         else:
-            for k in range(q_min, q_max+1):
+            for k in range(q_min[i-1], q_max[i-1]+1):
                 current_units = k
-                min_units = current_units - u_max
-                if min_units < q_min:
-                    min_units = q_min
-                max_units = current_units + i_max
-                if max_units > q_max:
-                    max_units = q_max
+                min_units = current_units - u_max[i]
+                if min_units < q_min[i]:
+                    min_units = q_min[i]
+                max_units = current_units + i_max[i]
+                if max_units > q_max[i]:
+                    max_units = q_max[i]
                 for j in range(min_units, max_units+1):
-                    addEdge(graph, vertices[i][k], vertices[i+1][j],round((j-k)*p_disc[i],2))
+                    addEdge(graph, vertices[i][k], vertices[i+1][j],round((j-k)*p_disc[i]+250,2))
+    for i in range (q_min[len(q_min)-1], q_max[len(q_min)-1]+1):
+        addEdge(graph, vertices[len(vertices)-1], vertices[len(vertices)-2][j], 0)
     return graph
 
-print(graph_dict())
+
+
+
+
+
+def optimization_of_gas_storage(graph , start , goal ): 
+    shortest_distance = {}
+    predecessor = {}
+    unseen_nodes = graph
+    path = []
+    infinity=9999
+    for node in unseen_nodes :
+        shortest_distance[node] = infinity
+    shortest_distance [ start ] = 0
+    while unseen_nodes : 
+        min_distance_node = None
+        for node in unseen_nodes : 
+            if min_distance_node is None :
+                min_distance_node = node
+            elif shortest_distance [node] < shortest_distance[min_distance_node]: 
+                min_distance_node = node
+        for child_node, weight in graph [ min_distance_node ].items ( ): 
+            if graph.get(child_node) is None:
+                pass
+            else:
+                if weight + shortest_distance [min_distance_node] < shortest_distance[child_node]: 
+                    shortest_distance[child_node] = weight + shortest_distance [ min_distance_node ]
+                    predecessor [ child_node ] = min_distance_node
+        unseen_nodes.pop(min_distance_node)
+    current_node = goal
+    while current_node != start:
+        path. insert (0,current_node)
+        current_node = predecessor[current_node]
+    path . insert (0 , start )
+    if shortest_distance[goal] != infinity:
+        longest_distance = (shortest_distance [ goal ] - 13 * 250) * (-1)
+        print(f'The_highest_profit_is {round(longest_distance,2)} Euro') 
+        print(f'The_path_is_ + {str(path)}')
+
+    
+optimization_of_gas_storage(graph_dict(), 'q0.5', 'q_slut')
 
 
 
